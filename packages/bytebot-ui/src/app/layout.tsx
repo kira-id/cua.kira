8c4e148a4305c058c80582205987f36ba6faa386
samkoesnadi <samuel@kira.id>
2025-10-26 05:25:43 +0700

    Changed logo (WIP for diverging repo)

--- packages/bytebot-ui/src/app/layout.tsx
+++ packages/bytebot-ui/src/app/layout.tsx
@@ -6,8 +6,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Bytebot",
-  description: "Bytebot is the container for desktop agents.",
+  title: "CUA.Kira",
+  description: "CUA.Kira is AI for desktop agents",
 };
 
 export default function RootLayout({
